198233de4aea04a708832f24433683d4d6268bdb
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-06 09:48:43 +0000

    commit

---
+++ 10/10_03.js
@@ -0,0 +1,23 @@
+let りんご = {
+    name: 'りんご',
+　　color: 'red',
+　　size: 28.0,
+　　weight: 400
+};
+
+let みかん = {
+    name: 'みかん',
+    color: 'orange',
+    size: 5.5,
+    weight: 130
+};
+
+let ぶどう = {
+    name: 'ぶどう',
+    color: 'purple',
+    size: 13.5,
+    weight: 295
+};
+
+let brother = [りんご, みかん, ぶどう];
+
