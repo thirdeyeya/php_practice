70739a382ed18a9fc5af5cc0e1536c6a40f6bef0
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-06 11:51:03 +0000

    commit

---
+++ 11/11/11_16.js
@@ -0,0 +1,7 @@
+let e1 = document.getElementById('p1');
+let e2 = document.getElementById('p2');
+let e3 = document.getElementById('p3');
+
+e1.innerText = 'テキストの部分の文字列';
+e2.innerHTML = '<p>HTMLの文字列</p';
+e3.outerHTML = '<h1>変更したタグ</h1>';
---
+++ 11/11/11_18.js
@@ -0,0 +1,6 @@
+let e1 = document.getElementBtId('p1');
+let e2 = document.getElementById('p2');
+let e3 = document.getElementById('p3');
+
+e2.innerText = '<p>テキスト文字列</p>'
+e3.id = 'p5';
