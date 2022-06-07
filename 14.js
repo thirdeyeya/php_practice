b75b52318435ebe9b9c0e10799fbc197e082d073
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-07 11:25:49 +0000

    commit

---
+++ 14.js
@@ -0,0 +1,13 @@
+try {
+    var a = [1,2,3];
+    
+    console.log(a[i]);
+} catch (i) {
+    if (i 1) {
+        console.log(a[1]);
+    } else if (i 2) {
+        console.log(a[2]);
+    } else if (i 3) {
+        console.log(a[3]);
+    }
+}
