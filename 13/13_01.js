1a371cfb0ce4ca78b3e39d8019c3ffeed3a78ad8
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-07 10:32:17 +0000

    commit

--- 13/13_01.js
+++ 13/13_01.js
@@ -6,7 +6,39 @@
       '子ども家具'
    ];
    
+   
+   let cate2 = [
+       
+       ['---'],
+       
+       ['ベッド','ソファ','棚・ラック','テーブル・椅子'],
+       
+       ['ベッド','寝具','マットレス'],
+       
+       ['家具・ラック','収納システム'],
+       
+       ['子供部屋家具','ベビー家具・ベビーグッズ']
+       
+    ];
+    
+    var itemList = [
+        { id: '0001', name: 'ソファベッド', tags: ['ベッド', 'ソファ', '寝具'] },
+        { id: '0002', name: 'シングルベッド', tags: ['ベッド', 'ソファ', '寝具'] },
+        { id: '0003', name: '子ども用ベッド', tags: ['ベッド','ソファ', '寝具', '子ども部屋家具'] },
+        { id: '0004', name: 'ソファ', tags: ['ソファ'] },
+        { id: '0005', name: 'キューブボックス', tags: ['家具・ラック', '収納システム'] },
+        { id: '0006', name: 'オープンラック', tags: ['家具・ラック', '収納システム'] },
+        { id: '0007', name: 'コンピュータデスク', tags: ['テーブル・椅子'] },
+        { id: '0008', name: 'サイドテーブル', tags:['テーブル・椅子'] },
+        { id: '0009', name: 'ダイニングテーブル', tags: ['テーブル・椅子'] },
+        { id: '0010', name: '子ども部屋用収納', tags: ['収納システム', '子ども部屋家具'] },
+        ];
+        
    let cate1Element = document.getElementById('mainMenu');
+   
+   let cate2Element = document.getElementById('subMenu');
+   
+   var itemListElement = document.getElementById('itemList');
    
    function setMainMenu() {
    
@@ -25,45 +57,11 @@
    }
    
    
-   
-   let cate2 = [
-       
-       ['---'],
-       
-       ['ベッド','ソファ','棚・ラック','テーブル・椅子'],
-       
-       ['ベッド','寝具','マットレス'],
-       
-       ['家具・ラック','収納システム'],
-       
-       ['子供部屋家具','ベビー家具・ベビーグッズ']
-       
-    ];
-    
-    let cate2Element = document.getElementById('subMenu');
-    
-    function setSubMenu() {
-        
-        cate2Element.innerHTML = '';
-        
-        for (let i = 0, i < cate2[1].length; i++) {
-            
-            let option = document.createElement('option');
-            option.value = cate2[1][i];
-            
-            option.text = cate2[1][i];
-            
-            cate2Element.appendChild(option);
-        }
-    }
-    
-   
-    
     function setSubMenu(idx) {
         
         cate2Element.innerHTML = '';
         
-        for (let i = 0; i < cate2[idx].length; i++) {
+        for (let i = 0, i < cate2[1].length; i++) {
             
             let option = document.createElement('option');
             option.value = cate2[idx][i];
@@ -74,31 +72,40 @@
         }
     }
     
-    
-    
-    setSubMenu(1);
-    
-    cate1Element.addEventListener('charge', function(){
+    function viewItemList(tag) {
+            itemListElement.innerHTML = '';
+            for (let i = 0; i < itemList.length; i++) {
+                if (itemList[i].tags.some(t => t === tag)) {
+                    
+                    let li = document.createElement('li');
+                    
+                    let text = document.createTextNode(itemList[i].id + ':' + itemList[i].name);
+                    
+                    li.appendChild(text);
+                    itemListElement.appendChild(li);
+                }
+            }
+    }
         
-        let idx = cate1Element.selectedIndex;
-        console.log(idx);
-    });
-    
     cate1Element.addEventListner('change', function(){
-        
-        let idx = cate1Element.selectedIndex;
-        
-        setSubMenu(idx);
+            
+            var idx = cate1Element.selectedIndex;
+            
+            setSubMenu(idx);
+            
+            viewItemList(cate2[idx][0]);
     });
     
-    cate2Element.addEventListner('change', function(){
-        
-        let idx = cate2Element.selectedIndex;
-        
-        let val = cate2Element.value;
-        
-        console.log(idx);
-        console.log(val);
+   
+    cate2Element.addEventListener('change', function(){
+            
+        var val = cate2Element.value;
+        viewItemList(val);
     });
-    
+        
+        
     setMainMenu();
+    
+    
+    
+        
