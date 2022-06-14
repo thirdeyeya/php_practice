d374013acbfe03f6ef889fdc1f9f91c560d253d5
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-14 07:08:15 +0000

    commit

--- 11/11/11_18.js
+++ 11/11/11_18.js
@@ -2,5 +2,5 @@ let e1 = document.getElementBtId('p1');
 let e2 = document.getElementById('p2');
 let e3 = document.getElementById('p3');
 
-e2.innerText = '<p>テキスト文字列</p>'
+e2.innerText = 'テキスト文字列'
 e3.id = 'p5';
