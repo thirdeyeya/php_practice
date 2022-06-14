d8312df9cceb19138a8fe0b36ed4525f07674e17
Sakika Tsutsui <longlongjourney@outlook.jp>
2022-06-14 07:34:11 +0000

    commit

--- 11/11/11_18.js
+++ 11/11/11_18.js
@@ -2,5 +2,6 @@ let e1 = document.getElementBtId('p1');
 let e2 = document.getElementById('p2');
 let e3 = document.getElementById('p3');
 
+e1.innerHTML = '<input type="checkbox" name="kamoku" value="国語">国語'
 e2.innerText = 'テキスト文字列'
 e3.id = 'p5';
